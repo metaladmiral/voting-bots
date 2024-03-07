e344197eb06d23c9b846bf9d62d09a43ae46a8d1
Metaladmiral <thesecretinspiration@gmail.com>
2024-03-08 02:48:04 +0530

    First

---
+++ syok.my 8march/vote.py
@@ -0,0 +1,87 @@
+from random import randint
+import time
+from fake_useragent import UserAgent
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+import logging
+
+# options = Options()
+# options.add_argument("-headless")
+
+class app:
+    def __init__(self) -> None:
+        # self.main_url = "https://my.syok.my/charts/my-music-chart-2020/my-20chart-186"        
+        self.main_url = "https://google.com"        
+        self.first_run = True
+        # self.total_captcha_err = 0
+        self.run()
+
+    def open_browser(self) :
+
+        options = Options()
+
+        ua = UserAgent()
+        user_agent = ua.random
+
+        service = Service(executable_path=r"C:\Users\mypra\Downloads\geckodriver.exe")
+        profile = webdriver.FirefoxProfile()
+        profile.set_preference("general.useragent.override", user_agent)
+        profile.set_preference("dom.webdriver.enabled", False)
+        profile.set_preference('useAutomationExtension', False)
+        profile.set_preference("browser.link.open_newwindow", 3)
+        profile.set_preference("browser.link.open_newwindow.restriction", 2)
+        profile.update_preferences()
+        
+        options.profile = profile
+        options.add_argument("--width=800")
+        options.add_argument("--height=600")
+        options.add_argument('--disable-blink-features=AutomationControlled')
+        # options.add_argument("-headless")
+
+        self.driver = webdriver.Firefox(service=service, options=options)
+
+    def runTab(self):
+        time.sleep(3)
+        print('trying')
+        try:
+            if(self.first_run):
+                self.driver.get(self.main_url)
+                self.first_run = False
+            else:
+                self.driver.execute_script('''window.open("","_blank");''')
+                previous_window = self.driver.window_handles[0]
+                new_window = self.driver.window_handles[-1]
+                time.sleep(3)
+                self.driver.switch_to.window(previous_window)
+                self.driver.close()
+
+                time.sleep(3)
+
+                self.driver.switch_to.window(new_window)
+                self.driver.get(self.main_url)
+
+                time.sleep(2)
+        
+            self.driver.find_element(By.XPATH, '//*[@id="p_lt_Content_pageplaceholder_p_lt_ctl02_Repeater_repItems_ctl00_ctl00_VoteBtn"]').click()
+
+        except Exception as e:
+            print(e)
+
+        self.runTab()
+
+    def run(self):
+        self.open_browser()
+        time.sleep(2)
+        self.runTab()
+
+
+
+app()
